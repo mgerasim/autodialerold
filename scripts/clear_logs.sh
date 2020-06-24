2801b28f8ebcb16e935841c7c27cbc7a13192930
mgerasim <mgerasim@inbox.ru>
2020-06-24 16:20:45 +1000

    Create clear_logs.sh

---
+++ scripts/clear_logs.sh
@@ -0,0 +1,15 @@
+# sudo chmod 777 -R /var/log/asterisk/
+# sudo chmod 777 -R /var/log/
+
+rm -rf /var/log/asterisk/cdr-csv/
+rm -rf /var/log/asterisk/messages-*
+rm -rf /var/log/asterisk/messages.*
+
+rm -rf /var/log/asterisk/messages
+rm -rf /var/log/asterisk/cdr-csv/
+rm -rf /var/log/asterisk/security
+
+rm -f /home/rails/projects/autodialerold/log/*.log
+rm -f /home/rails/apps/autodialerold/current/log/*.log
+rm -rf /var/log/*-*
+rm -rf /home/rails/projects/autodialerold/log/*.log
