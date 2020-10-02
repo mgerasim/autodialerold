650d3ac2319a984d5926915c4ef58e51e1938285
Mikhail Gerasimov <mgerasim@inbox.ru>
2020-10-02 03:06:54 +0300

    check-setting

--- db/migrate/20200920224839_update_version.rb
+++ db/migrate/20200920224839_update_version.rb
@@ -1,6 +1,7 @@
 class UpdateVersion < ActiveRecord::Migration[5.2]
   def change
     setting = Setting.first
+    setting = Setting.create if setting == nil
     setting.version = 3
     setting.save
   end
